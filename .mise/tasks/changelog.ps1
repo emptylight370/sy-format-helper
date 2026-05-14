753969d53393dc9dd6435f03067fcd566c92fbff
Emptylight <1378990254@qq.com>
2026-05-14 15:32:16 +0800

    build: 修正提取版本号脚本

--- .mise/tasks/changelog.ps1
+++ .mise/tasks/changelog.ps1
@@ -1,7 +1,7 @@
 #!/usr/bin/env pwsh
 #MISE description="Generate changelog"
 
-$version = (Get-Content theme.json | ConvertFrom-Json).version
+$version = (Get-Content plugin.json | ConvertFrom-Json).version
 git cliff -t $version
 prettier -w changelog.md
 git add changelog.md
